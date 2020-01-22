e832687ea813207258ea18446f31e727e478f873
Nicola Febbrari <febbrari@gmail.com>
2020-01-22 13:49:08 +0100

    Second iteration

--- coffeMachineTs/app/index.ts
+++ coffeMachineTs/app/index.ts
@@ -1,9 +1,11 @@
 type KindType = 'tea' | 'chocolate' | 'coffee';
 type KindMessageType = 'T' | 'H' | 'C';
 
+
 export type DrinkOrder = {
     kind: KindType;
     sugar: 0 | 1 | 2 | 3 | 4 | 5;
+    money: number;
 }
 type CoffeMachineServiceType = {
     make: (order: DrinkOrder) => string;
@@ -20,17 +22,32 @@ function getMessageKind(order: DrinkOrder): KindMessageType {
             return 'C'
     }
 }
+function getPrice(order: DrinkOrder): number {
+    switch (order.kind) {
+        case 'tea':
+            return 0.4;
+        case 'chocolate':
+            return 0.5;
+        case 'coffee':
+            return 0.6
+    }
+}
 
 
 export const coffeMachineService = (): CoffeMachineServiceType => {
+    const sendMessage = (kind: string, message: string): string => {
+        return `${kind}:${message}`;
+    };
     return {
         make: (order): string => {
+            const price = getPrice(order);
+            if (order.money < price) {
+                return `M:${(price - order.money).toFixed(2)} money missing`
+            }
             const messageOrderKind = getMessageKind(order);
-            return `${messageOrderKind}:${order.sugar ? order.sugar : ''}:${order.sugar ? 0 : ''}`
+            return sendMessage(messageOrderKind, `${order.sugar ? order.sugar : ''}:${order.sugar ? 0 : ''}`);
         },
-        sendMessage: (message): string => {
-            return `M:${message}`
-        }
+        sendMessage: (message): string => sendMessage('M', message)
     }
 }
 
--- coffeMachineTs/test/index.test.ts
+++ coffeMachineTs/test/index.test.ts
@@ -1,29 +1,30 @@
 import { coffeMachineService, DrinkOrder } from "../app";
 
-describe('coffeMachineService loginc', () => {
+describe('coffeMachineService logic', () => {
     it('should exist', () => {
         expect(coffeMachineService).toBeDefined();
     });
     it('should have a make function', () => {
         expect(coffeMachineService().make).toBeInstanceOf(Function);
     });
+
     it('should take order of tea', () => {
         const teaOrder: DrinkOrder = {
-            kind: 'tea', sugar: 1
+            kind: 'tea', sugar: 1, money: 0.4
         }
         const msgMakeTea = coffeMachineService().make(teaOrder)
         expect(msgMakeTea).toEqual('T:1:0');
     });
     it('should take order of chocolate', () => {
         const chocoOrder: DrinkOrder = {
-            kind: 'chocolate', sugar: 0
+            kind: 'chocolate', sugar: 0, money: 0.5
         }
         const msgMakeCoco = coffeMachineService().make(chocoOrder)
         expect(msgMakeCoco).toEqual('H::');
     });
     it('should take order of coffee', () => {
         const chocoOrder: DrinkOrder = {
-            kind: 'coffee', sugar: 2
+            kind: 'coffee', sugar: 2, money: 0.6
         }
         const msgMakeCoffe = coffeMachineService().make(chocoOrder)
         expect(msgMakeCoffe).toEqual('C:2:0');
@@ -33,5 +34,32 @@ describe('coffeMachineService loginc', () => {
         const msgContent = coffeMachineService().sendMessage('message-content')
         expect(msgContent).toEqual('M:message-content');
     });
-
+    it('should take order of choco, money ok', () => {
+        const chocoOrder: DrinkOrder = {
+            kind: 'coffee', sugar: 2, money: 0.6
+        }
+        const msgMakeCoffe = coffeMachineService().make(chocoOrder)
+        expect(msgMakeCoffe).toEqual('C:2:0');
+    });
+    it('should reject order of coffee, money missing', () => {
+        const chocoOrder: DrinkOrder = {
+            kind: 'coffee', sugar: 2, money: 0.4
+        }
+        const msgMakeCoffe = coffeMachineService().make(chocoOrder)
+        expect(msgMakeCoffe).toEqual(`M:0.20 money missing`);
+    });
+    it('should reject order of choco, money missing', () => {
+        const chocoOrder: DrinkOrder = {
+            kind: 'chocolate', sugar: 2, money: 0.4
+        }
+        const msgMakeCoffe = coffeMachineService().make(chocoOrder)
+        expect(msgMakeCoffe).toEqual(`M:0.10 money missing`);
+    });
+    it('should reject order of tea, money missing', () => {
+        const chocoOrder: DrinkOrder = {
+            kind: 'tea', sugar: 2, money: 0.3
+        }
+        const msgMakeCoffe = coffeMachineService().make(chocoOrder)
+        expect(msgMakeCoffe).toEqual(`M:0.10 money missing`);
+    });
 });
